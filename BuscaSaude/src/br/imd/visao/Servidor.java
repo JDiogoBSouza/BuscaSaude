b77826c82caa6be1a6ab1eefc67d09a1b7c779f1
Diogo <Diogo@Diogo-PC>
2018-08-29 21:37:49 -0300

    Tratamento de Exceções no Servidor
    Adição de Mensagens nas exceções que podem ser ocorridas no servidor.

--- BuscaSaude/src/br/imd/visao/Servidor.java
+++ BuscaSaude/src/br/imd/visao/Servidor.java
@@ -12,16 +12,29 @@ import br.imd.modelo.Services;
  */
 public class Servidor 
 {
-	public static void main(String[] args) throws RemoteException, MalformedURLException {
+	public static void main(String[] args) {
 		
-		// Execução do modulo de referencia remota (RMI Registry)
-		LocateRegistry.createRegistry(1099);
+		try {
+			
+			// Execução do modulo de referencia remota (RMI Registry)
+			LocateRegistry.createRegistry(1099);
+			
+			// Instanciacao do servene
+			Services services = new Services();
+			
+			// Registro do servente no modulo de referencia remota (RMI Registry)
+			Naming.rebind("rmi://localhost/Services", services);
+			System.out.println("Servidor pronto e registrado no RMI Registry.");
+			
+		}
+		catch( MalformedURLException e ) {
+			System.out.println("Erro: URL do servidor inválida.");
+			//e.printStackTrace();
+		}
+		catch( RemoteException e ) {
+			System.out.println("Erro: Falha na execução remota.");
+			//e.printStackTrace();
+		}
 		
-		// Instanciacao do servene
-		Services services = new Services();
-		
-		// Registro do servente no modulo de referencia remota (RMI Registry)
-		Naming.rebind("rmi://localhost/Services", services);
-		System.out.println("Servidor pronto e registrado no RMI Registry.");
 	}
 }
